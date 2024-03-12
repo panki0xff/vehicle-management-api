e7d23add59803251a7c901c203a225104e091abf
Benjamin <panki0xff@gmail.com>
2024-03-12 11:25:41 +0000

    Initial commit

---
+++ src/main/java/com/theaa/exercise/vehiclemanagementapi/VehicleManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.theaa.exercise.vehiclemanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleManagementApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/theaa/exercise/vehiclemanagementapi/VehicleManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theaa.exercise.vehiclemanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
